14c45baf19c162fde9faea1ef4046a178ff8552d
Robert Whittaker <cyclistsdismount@gmail.com>
2012-07-31 20:14:45 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mesprit::Application.config.secret_token = 'db018264abcbd872ac1edee4054d92439f50fc3287ee219fa2f662b43ed4c658ed911a6f02d1e5171ba6a8641da8e77784cf4a409e96e2fc32c76bd1e85d8390'
